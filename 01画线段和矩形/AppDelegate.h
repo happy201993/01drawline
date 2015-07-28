60c75eedab4e78e7959c54e4c55773ca15a02fca
yang <15757176639@163.com>
2015-07-28 16:09:43 +0800

    Initial Commit

---
+++ 01画线段和矩形/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01画线段和矩形
+//
+//  Created by 杨少伟 on 15/7/28.
+//  Copyright (c) 2015年 YCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 01画线段和矩形/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01画线段和矩形
+//
+//  Created by 杨少伟 on 15/7/28.
+//  Copyright (c) 2015年 YCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
